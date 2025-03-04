9998f581512376cc274143cad65b9db06702d244
MorganNwt <metodik60@hotmail.fr>
2025-03-04 15:42:45 +0100

    feat(components) : add banner, carescale, footer, plantItem and shoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/shoppingList.css'
 
 function ShoppingList() {
@@ -8,21 +9,26 @@ function ShoppingList() {
         []
     )
 
-	return <div>
+	return (
+            <div>
                 <ul>
                     {categories.map((cat) => (
                         <li key={cat}>{cat}</li>
                     ))}
                 </ul>
-                <ul>
-                    {plantList.map((plant) => (
-                        <li key={plant.id}>
-                            {plant.name}
-                            {plant.isBestSale && <span>🔥</span>}
-                        </li>
+                <ul classeName='lmj-plant-list'>
+                    {plantList.map(({id, cover, name, water, light}) => (
+                       <PlantItem
+                            id={id}
+                            cover={cover}
+                            name={name}
+                            water={water}
+                            light={light}
+                       />
                     ))}
                 </ul>
             </div>
+        )    
 }
 
 export default ShoppingList
